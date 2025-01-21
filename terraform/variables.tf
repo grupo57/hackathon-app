77b56e5e6e908f44daee1662205101618ef16d72
Fábio Peixoto <fabio.silva@empresta.com.br>
2025-01-21 20:58:37 -0300

    feat: Add pipeline deploy

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "lambda_function_name" {
+  default = "hackathon-app-api"
+}
+
+variable "api_gateway_name" {
+  default = "hackathon-app-api-gateway"
+}
+
+variable "s3_bucket_name" {
+  default = "hackathon-app-api-deploy-bucket"
+}
