d980865d9200fda48b903018ff7aeb5c9d9d2dbb
Fábio Peixoto <fabio.silva@empresta.com.br>
2025-01-26 18:13:59 -0300

    fix: Ajuste

--- src/main/java/br/com/fiap/soat07/hackathon/LambdaHandler.java
+++ src/main/java/br/com/fiap/soat07/hackathon/LambdaHandler.java
@@ -3,12 +3,13 @@ package br.com.fiap.soat07.hackathon;
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class LambdaHandler implements RequestHandler<AwsProxyRequest, AwsProxyResponse> {
 
-    private static final SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
 
     static {
         try {
